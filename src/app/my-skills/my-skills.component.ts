763c7cbd03fcc3e6c6aec31c8a7e8927c8deae49
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-06-13 22:56:46 +0200

    styled my skills comp

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
-
+  public logosContent: {logos: string, description: string}[] = [
+    { logos: '../../assets/img/angular.png', description: 'Angular'},
+    { logos: '../../assets/img/ts.png', description: 'TypeSript'},
+    { logos: '../../assets/img/js.png', description: 'JavaScript'},
+    { logos: '../../assets/img/html.png', description: 'HTML'},
+    { logos: '../../assets/img/fb.png', description: 'Firebase'},
+    { logos: '../../assets/img/git.png', description: 'GIT'},
+    { logos: '../../assets/img/css.png', description: 'CSS'},
+    { logos: '../../assets/img/api.png', description: 'Rest-Api'},
+    { logos: '../../assets/img/scrum.png', description: 'Scrum'},
+    { logos: '../../assets/img/md.png', description: 'Material design'},
+  ];
 }

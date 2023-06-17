36eee23f120958c35ffa1850a2d794c46bf45cc9
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-06-17 17:48:43 +0200

    added some link destinations

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,8 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  displayEnterName = false;
+  displayEnterEmail = false;
+  displayEnterMessage = false;
+
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+
+  showEnterName() {
+    this.displayEnterName = true;
+  }
+
+  hideEntername() {
+    this.displayEnterName = false;
+  }
+
+  showEnterEmail() {
+    this.displayEnterEmail = true;
+  }
+
+  hideEnterEmail() {
+    this.displayEnterEmail = false;
+  }
+
+  showEnterMessage() {
+    this.displayEnterMessage = true;
+  }
+
+  hideEnterMessage() {
+    this.displayEnterMessage = false;
+  }
+
+
+  sendMail() {
+
   }
 }

4fb20dcbd5de8f18ea93d77f21be5280909871ee
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-07-18 12:38:05 +0200

    fixed some SCSS styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
+
 const routes: Routes = [
   { path: '', component: MainComponent},
   { path: 'imprint', component: ImprintComponent},
